5cf08ad603e49627bfc66ff0159da9c306a72d9a
Jean-Luc Aufranc <cnxsoft@cnx-software.com>
2018-03-11 14:17:28 +0700

    Initial commit

---
+++ functions.php
@@ -0,0 +1,35 @@
+<?php
+function my_theme_enqueue_styles() {
+
+    $parent_style = 'parent-style'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
+
+    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array( $parent_style )
+    );
+}
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+
+/* JLA - Set excerpts length to 160 words */
+function custom_excerpt_length( $length ) {
+	return 160;
+}
+add_filter( 'excerpt_length', 'custom_excerpt_length', 999 );
+
+function catch_that_image() {
+  global $post, $posts;
+  $first_img = '';
+  ob_start();
+  ob_end_clean();
+  $output = preg_match_all('/<img.+src=[\'"]([^\'"]+)[\'"].*>/i', $post->post_content, $matches);
+  $first_img = $matches [1] [0];
+ 
+  if(empty($first_img)){ //Defines a default image
+    $first_img = "/images/default.jpg";
+  }
+  return $first_img;
+}
+
+?>
+
---
+++ template-parts/post/content.php
@@ -0,0 +1,109 @@
+<?php
+/**
+ * Template part for displaying posts
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.2
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<?php
+	if ( is_sticky() && is_home() ) :
+		echo twentyseventeen_get_svg( array( 'icon' => 'thumb-tack' ) );
+	endif;
+	?>
+	<header class="entry-header">
+		<?php
+		if ( 'post' === get_post_type() ) {
+			echo '<div class="entry-meta">';
+				if ( is_single() ) {
+					twentyseventeen_posted_on();
+				} else {
+					echo twentyseventeen_time_link();
+					twentyseventeen_edit_link();
+				};
+			echo '</div><!-- .entry-meta -->';
+		};
+
+		if ( is_single() ) {
+			the_title( '<h1 class="entry-title">', '</h1>' );
+		} elseif ( is_front_page() && is_home() ) {
+			the_title( '<h3 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h3>' ); ?>
+			<img alt="" align="left" width=300 height=250 margin=10px src="<?php echo catch_that_image(); ?> ">
+			<?php 
+		} else {
+			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' ); ?>
+			<img alt="" align="left" width=300 height=250 margin=10px src="<?php echo catch_that_image(); ?> "> 
+			<?php
+		}
+		?>
+	</header><!-- .entry-header -->
+
+	<?php if ( '' !== get_the_post_thumbnail() && ! is_single() ) : ?>
+		<div class="post-thumbnail">
+			<a href="<?php the_permalink(); ?>">
+				<?php the_post_thumbnail( 'twentyseventeen-featured-image' ); ?>
+			</a>
+		</div><!-- .post-thumbnail -->
+	<?php else :
+	endif; ?>
+
+	<div class="entry-content">
+		<?php
+
+		if ( is_single() ) {
+		/* JLA */
+	        echo adrotate_ad(1);
+
+		/* translators: %s: Name of current post */
+		the_content( sprintf(
+			__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'twentyseventeen' ),
+			get_the_title()
+		) );
+		} else {
+
+		the_excerpt( sprintf(
+			__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'twentyseventeen' ),
+			get_the_title()
+		) );
+		}
+        
+
+		wp_link_pages( array(
+			'before'      => '<div class="page-links">' . __( 'Pages:', 'twentyseventeen' ),
+			'after'       => '</div>',
+			'link_before' => '<span class="page-number">',
+			'link_after'  => '</span>',
+		) );
+		?>
+	</div><!-- .entry-content -->
+
+	<?php
+	if ( is_single() ) { ?>
+		<!-- JLA - Ad related content -->
+                <table>
+                <tr>
+                        <td width=338>
+		        <?php echo adrotate_ad(2); ?>
+                        </td>
+                        <td valign="top">
+                        <?php
+                                if(function_exists('related_posts')) {
+                                        related_posts();
+                                }
+                        ?>
+                        </td>
+                </tr>
+                </table>
+
+		<?php twentyseventeen_entry_footer();
+	}
+	?>
+
+</article><!-- #post-## -->
