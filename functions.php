08c957fd483cc119f4b3742705721fa67b390b59
Jean-Luc Aufranc <cnxsoft@cnx-software.com>
2019-06-11 21:43:30 +0700

    Allow contributors to upload media

--- functions.php
+++ functions.php
@@ -83,6 +83,14 @@ add_filter( 'og_og_description_value', function( $value ) {
     return wp_trim_words( $value );
 });
 
+/* Let Contributor Role to Upload Media */
+if ( current_user_can('contributor') && !current_user_can('upload_files') )
+    add_action('admin_init', 'allow_contributor_uploads');
+function allow_contributor_uploads() {
+    $contributor = get_role('contributor');
+    $contributor->add_cap('upload_files');
+}
+
 /* Add Meta Referrer Tag in Header without Plugin
 function add_meta_tags() {
 ?>
