82b3db0d9201c354d9f74bca124eb328bd41cd59
cnxsoft <cnxsoft@cnx-software.com>
2021-09-19 21:33:22 +0700

    Remove Proudly sponsored by Wordpress

---
+++ template-parts/footer/site-info.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Displays footer site info
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since Twenty Seventeen 1.0
+ * @version 1.0
+ */
+
+?>
+<div class="site-info">
+	<?php
+	if ( function_exists( 'the_privacy_policy_link' ) ) {
+		the_privacy_policy_link( '', '<span role="separator" aria-hidden="true"></span>' );
+	}
+	?>
+</div><!-- .site-info -->
