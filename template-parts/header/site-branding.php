bb49ac5cb1f707398bedab2f9f58a37f37762505
cnxsoft <cnxsoft@cnx-software.com>
2024-12-28 21:32:11 +0700

    Rename header-top to header-toop, another try...

--- template-parts/header/site-branding.php
+++ template-parts/header/site-branding.php
@@ -34,7 +34,7 @@
                 <!-- JLA - PubGalaxy Top Banner -->
                 <?php 
                 if(function_exists('adrotate_group')) {
-                        echo '<div class="header-top">';
+                        echo '<div class="header-toop">';
                         if (!wp_is_mobile()) {
                	            echo adrotate_group(1);
                         } else {
